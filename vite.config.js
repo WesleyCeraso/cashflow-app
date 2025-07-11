44816ef39c551d227aec0a48987fd1e705ecd614
Wesley Prudencio <wesleyceraso@gmail.com>
2025-07-11 21:35:00 +0000

    Configure for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/cashflow-app/',
   server: {
     allowedHosts: ["dev.leycera.dedyn.io"],
     host: '0.0.0.0',
